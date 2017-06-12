e718221164cd066afb0ec30209f633099ad95253
Nicolas Daubresse <nicolas.daubresse@gmail.com>
2017-06-12 10:19:20 +0200

    Cleaning a bit and adding domains and IP

--- VT_checker.py
+++ VT_checker.py
@@ -10,16 +10,61 @@ from optparse import OptionParser
 # Tool version
 VERSION = '0.1'
 
+
+def submit_resource(resource_type, uri, resource_file, api_key):
+  if not os.path.isfile(resource_file):
+    print("\033[91m[ERROR]\033[0m Given file does not exist: " + resource_file)
+    sys.exit(-1)
+  with open(resource_file) as resource:
+    for re in resource:
+      params = {'apikey': api_key, 
+        'resource': re}
+      r = requests.post('https://www.virustotal.com/vtapi/v2' + uri,
+        data=params)
+      while r.status_code == 204:
+        r = requests.post('https://www.virustotal.com/vtapi/v2' + uri,
+          data=params)
+      if r.status_code == 403:
+        print("\033[93m[WARNING]\033[0m Got 403 status code when submitting" +
+          " resource: " + re)
+      else:
+        try: 
+          json_response = r.json()
+          if json_response['response_code'] == 1:
+            print("\033[94m[" + resource_type + "]\033[0m " +
+              re.replace('\n', '') + ":")
+            print("\tLast scan date: " + json_response['scan_date'])
+            if json_response['positives'] == 0:
+              print("\tDetection ratio: \033[92m" + 
+                str(json_response['positives']) + "/" + 
+                str(json_response['total']) + "\033[0m\n")
+            else:
+              print("\tDetection ratio: \033[91m" + 
+                str(json_response['positives']) + "/" + 
+                str(json_response['total']) + "\033[0m\n")
+          else :
+            print("\033[94m[" + resource_type + "]\033[0m " +
+              re.replace('\n', '') + ":")
+            print("\t" + json_response['verbose_msg'] + "\n")
+        except:
+          print("\033[93m[WARNING]\033[0m Got invalid response format for " +
+            "resource: " + re)
+
+
 def main(argv=None):
   parser = OptionParser()
   parser.add_option("-k", "--key", dest="api_key", default=None,
     help="Api Key for VirusTotal submission.")
   parser.add_option("-u", "--urls", dest="urls", default=None,
     help="File containing the URLs to submit.")
+  parser.add_option("-d", "--domains", dest="domains", default=None,
+    help="File containing the domains to submit.")
+  parser.add_option("-i", "--ips", dest="ips", default=None,
+    help="File containing the IPs to submit.")
+  parser.add_option("-m", "--hashes", dest="hashes", default=None,
+    help="File containing the hashes to submit.")
   parser.add_option("-f", "--folder", dest="folder", default=None,
     help="Folder containing the files to submit (by MD5).")
-  parser.add_option("-m", "--md5", dest="hashes", default=None,
-    help="File containing the hashes to submit.")
   (options, args) = parser.parse_args()
 
   if not options.api_key:
@@ -30,38 +75,18 @@ def main(argv=None):
     sys.exit(-1)
 
   if options.urls:
-    if not os.path.isfile(options.urls):
-      print("\033[91m[ERROR]\033[0m Given file of URLs does not exist.")
-      sys.exit(-1)
-    with open(options.urls) as u:
-      for url in u:
-        params = {'apikey': options.api_key, 
-          'resource': url}
-        r = requests.post('https://www.virustotal.com/vtapi/v2/url/report',
-          data=params)
-        while r.status_code == 204:
-          r = requests.post('https://www.virustotal.com/vtapi/v2/url/report',
-            data=params)
-        if r.status_code == 403:
-          print("\033[93m[WARNING]\033[0m Got 403 status code when submitting" +
-            " url: " + url)
-        else:
-          try: 
-            json_response = r.json()
-            print("\033[94m[URL]\033[0m " + url.replace('\n', '') + ":")
-            print("\tLast scan date: " + json_response['scan_date'])
-            if json_response['positives'] == 0:
-              print("\tDetection ratio: \033[92m" + 
-                str(json_response['positives']) + "/" + 
-                str(json_response['total']) + "\033[0m\n")
-            else:
-              print("\tDetection ratio: \033[91m" + 
-                str(json_response['positives']) + "/" + 
-                str(json_response['total']) + "\033[0m\n")
-          except:
-            print("\033[93m[WARNING]\033[0m Got invalid response format for " +
-              "url: " + url)
-            
+    submit_resource("URL", "/url/report", options.urls, options.api_key)
+                
+  if options.domains:
+    submit_resource("DOMAIN", "/domain/report", options.domains, 
+      options.api_key)
+
+  if options.ips:
+    submit_resource("IP", "/ip-address/report", options.ips, options.api_key)
+
+  if options.hashes:
+    submit_resource("HASH", "/file/report", options.hashes, options.api_key)
+
   if options.folder:
     if not os.path.isdir(options.folder):
       print("\033[91m[ERROR]\033[0m Given folder does not exist.")
@@ -83,54 +108,25 @@ def main(argv=None):
         else:
           try: 
             json_response = r.json()
-            print("\033[94m[FILE]\033[0m " + os.path.join(root,filename) + 
-              "(" + md5 + "):")
-            print("\tLast scan date: " + json_response['scan_date'])
-            if json_response['positives'] == 0:
-              print("\tDetection ratio: \033[92m" + 
-                str(json_response['positives']) + "/" + 
-                str(json_response['total']) + "\033[0m\n")
-            else:
-              print("\tDetection ratio: \033[91m" + 
-                str(json_response['positives']) + "/" + 
-                str(json_response['total']) + "\033[0m\n")
-          except:
-            print("\033[93m[WARNING]\033[0m Got invalid response format for " + 
-              "file: " + os.path.join(root,filename))
-
-  if options.hashes:
-    if not os.path.isfile(options.hashes):
-      print("\033[91m[ERROR]\033[0m Given file of hashes does not exist.")
-      sys.exit(-1)
-    with open(options.hashes) as hashes:
-      for h in hashes:
-        params = {'apikey': options.api_key, 
-          'resource': h}
-        r = requests.post('https://www.virustotal.com/vtapi/v2/file/report',
-          data=params)
-        while r.status_code == 204:
-          r = requests.post('https://www.virustotal.com/vtapi/v2/file/report',
-            data=params)
-        if r.status_code == 403:
-          print("\033[93m[WARNING]\033[0m Got 403 status code when submitting" +
-            " hash: " + h)
-        else:
-          try: 
-            json_response = r.json()
-            print("\033[94m[HASH]\033[0m " + h.replace('\n', '') + ":")
-            print("\tLast scan date: " + json_response['scan_date'])
-            if json_response['positives'] == 0:
-              print("\tDetection ratio: \033[92m" + 
-                str(json_response['positives']) + "/" + 
-                str(json_response['total']) + "\033[0m\n")
-            else:
-              print("\tDetection ratio: \033[91m" + 
-                str(json_response['positives']) + "/" + 
-                str(json_response['total']) + "\033[0m\n")
+            if json_response['response_code'] == 1:
+              print("\033[94m[FILE]\033[0m " + os.path.join(root,filename) + 
+                "(" + md5 + "):")
+              print("\tLast scan date: " + json_response['scan_date'])
+              if json_response['positives'] == 0:
+                print("\tDetection ratio: \033[92m" + 
+                  str(json_response['positives']) + "/" + 
+                  str(json_response['total']) + "\033[0m\n")
+              else:
+                print("\tDetection ratio: \033[91m" + 
+                  str(json_response['positives']) + "/" + 
+                  str(json_response['total']) + "\033[0m\n")
+            else :
+              print("\033[94m[FILE]\033[0m " + os.path.join(root,filename) + 
+                "(" + md5 + "):")
+              print("\t" + json_response['verbose_msg'] + "\n")
           except:
             print("\033[93m[WARNING]\033[0m Got invalid response format for " +
-              "hash: " + h)
-            
+              "resource: " + os.path.join(root,filename))
 
 if __name__ == '__main__':
   main()
